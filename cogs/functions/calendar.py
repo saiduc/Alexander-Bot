3938fd6dcee16e16c4a6af0760092870c741d0b3
SaiDuc <saipandian97@gmail.com>
2020-06-26 22:14:08 +0100

    Added greyed out weekends

--- cogs/functions/calendar.py
+++ cogs/functions/calendar.py
@@ -2,6 +2,7 @@ import calendar
 import numpy as np
 from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 plt.style.use("seaborn-dark")
 
 
@@ -10,8 +11,13 @@ def plot_calendar(days, months):
     # non days are grayed
 
     ax = plt.gca().axes
+    weekend_days, weekend_months = get_weekends(2020)
+
+    for i in range(len(weekend_days)):
+        ax.add_patch(Rectangle((weekend_days[i], weekend_months[i]), width=.9, height=.9, color='gray', alpha=.3))
+
     for d, m in zip(days, months):
-        ax.add_patch(Rectangle((d, m), 
+        ax.add_patch(Rectangle((d, m),
                                width=0.9, height=0.9, color='C0'))
 
     plt.yticks(np.arange(1, 13)+.5, list(calendar.month_abbr)[1:])
@@ -26,7 +32,8 @@ def plot_calendar(days, months):
     plt.tight_layout()
     plt.savefig("tmp.jpg")
 
-def getData(name):
+
+def get_data(name):
     names, dates = np.loadtxt("./data/log.dat", delimiter=",", unpack=True, dtype="str")
     data = []
     for i in range(len(names)):
@@ -39,3 +46,15 @@ def getData(name):
         months.append(int(item.split("-")[1]))
         days.append(int(item.split("-")[2]))
     return days, months
+
+
+def get_weekends(year):
+    weekend_day = []
+    weekend_month = [] 
+    start = datetime(year, 1, 1)
+    for i in range(365):
+        day_of_the_year = start + timedelta(days=i)
+        if day_of_the_year.weekday() > 4:
+            weekend_day.append(day_of_the_year.day)
+            weekend_month.append(day_of_the_year.month)
+    return weekend_day, weekend_month
