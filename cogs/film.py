31a8206ea90c844c111feeb0faa6a47ec62cbf08
SaiDuc <saipandian97@gmail.com>
2020-07-03 16:13:53 +0100

    Added watch list system

--- cogs/film.py
+++ cogs/film.py
@@ -9,6 +9,7 @@ class Film(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+
         if message.author == self.bot.user:
             return
 
@@ -18,39 +19,76 @@ class Film(commands.Cog):
                 name = message.content[1:-1]
                 embed_item = self.make_embed(name)
                 await message_channel.send(embed=embed_item)
+                # the movie is already added to list, but will be deleted if user answers no
+                msg = await message_channel.send("Add to Watch List?")
+                await msg.add_reaction("\U0001F44D")
+                await msg.add_reaction("\U0001F44E")
         else:
             return
 
-    def make_embed(self, name):
-        ia = IMDb()
-        top_result = ia.search_movie(name)[0]
-        movie = ia.get_movie(top_result.getID())
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        channel = await self.bot.fetch_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        user = await self.bot.fetch_user(payload.user_id)
+        emoji = payload.emoji
+        watchlist = "./data/watchlist.dat"
 
-        title = movie["title"]
-        year = movie["year"]
-        kind = movie["kind"].title()
-        genres = movie["genres"]
-        cover = movie["full-size cover url"]
-        outline = movie["plot outline"]
-        cast = movie["cast"][:4]
-        rating = movie["rating"]
-        language = movie["languages"][0]
-        runtimes = movie["runtimes"][0]
+        if user != self.bot.user:
+            if "Add to Watch List?" in str(message.content):
+                r = str(str(emoji).encode("unicode-escape").decode("ASCII"))
+                if r == r"\U0001f44d":
+                    await channel.send("Adding to Watch List!")
+                elif r == r"\U0001f44e":
+                    await channel.send("Not adding to Watch List!")
+                    with open(watchlist) as readFile:
+                        lines = readFile.readlines()
+                    with open(watchlist, "w") as writeFile:
+                        writeFile.writelines([item for item in lines[:-1]])
+
+
+    def make_embed(self, name):
+        watchlist = "./data/watchlist.dat"
+        ia = IMDb()
+        try:
+            top_result = ia.search_movie(name)[0]
+            movie = ia.get_movie(top_result.getID())
+            title = movie["title"]
+            year = movie["year"]
+            kind = movie["kind"].title()
+            genres = movie["genres"]
+            cover = movie["full-size cover url"]
+            outline = movie["plot outline"]
+            cast = movie["cast"][:4]
+            rating = movie["rating"]
+            language = movie["languages"][0]
+            runtimes = movie["runtimes"][0]
+        except:
+            pass
 
         embed = discord.Embed(title=title)
-        embed.set_thumbnail(url=cover)
-        embed.add_field(name="Synopsis", value=outline, inline=False)
-        embed.add_field(name="Cast", value=f"{cast[0]}, {cast[1]}, {cast[2]}, {cast[3]}", inline=False)
-        embed.add_field(name="General Details",
-                        value=f"Type: {kind}\n"
-                              f"Year: {year}\n"
-                              f"Genre: {genres[0]}",
-                        inline=True)
-        embed.add_field(name="Other Details",
-                        value=f"Score: {rating}\n"
-                              f"Language: {language}\n"
-                              f"Runtime: {runtimes} minutes",
-                        inline=True)
+
+        try:
+            embed.set_thumbnail(url=cover)
+            embed.add_field(name="Synopsis", value=outline, inline=False)
+            embed.add_field(name="Cast", value=f"{cast[0]}, {cast[1]}, {cast[2]}, {cast[3]}", inline=False)
+            embed.add_field(name="General Details",
+                            value=f"Type: {kind}\n"
+                                  f"Year: {year}\n"
+                                  f"Genre: {genres[0]}",
+                            inline=True)
+            embed.add_field(name="Other Details",
+                            value=f"Score: {rating}\n"
+                                  f"Language: {language}\n"
+                                  f"Runtime: {runtimes} minutes",
+                            inline=True)
+        except:
+            msg = "Unfortunately, I couldn't find the necessary information. Sorry!"
+            embed.add_field(name="Error", value=msg, inline=False)
+
+        with open(watchlist, "a") as myFile:
+            myFile.write(title + "\n")
+
         return embed
 
 
